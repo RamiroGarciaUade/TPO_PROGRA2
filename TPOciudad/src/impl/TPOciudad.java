0c8145ac7b5248b936805fa3f34b5e05e6d7ab97
MatiasMarano10 <matmarano@uade.edu.ar>
2024-06-12 15:28:04 -0300

    Metodos ciudadesVecinas, ciudadesPuente y ciudadesPredecesoras

--- TPOciudad/src/impl/TPOciudad.java
+++ TPOciudad/src/impl/TPOciudad.java
@@ -1,5 +1,6 @@
 package impl;
 
+import api.ColaStringTDA;
 import api.DiccionarioProvinciasTDA;
 import api.GrafoCiudadesTDA;
 import api.TPOciudadTDA;
@@ -16,24 +17,24 @@ public class TPOciudad implements TPOciudadTDA{
 
     public void listarProvincias() {
         metodosCola.mostrarProvinciaCiudad(diccionario);
-    } // MOSTAR PROVICIAS Y SUS CIUDADES
+    }
+
+    // MOSTAR PROVICIAS Y SUS CIUDADES
     public void listarCiudad(){
         metodosCola.mostrarCiudades(diccionario);
-    }// MOSTAR CIUDADES DE UNA PROVICIA
+    }
 
-    public void cargarCiudades(String provicia , String ciudad){
+    public void cargarCiudades(String provincia, String ciudad) {
         if (grafoCiudad == null) {
             diccionario.InicializarDiccionarioProvinciasTDA();
             grafoCiudad.InicializarGrafo();
+        }
 
-            diccionario.Agregar(provicia, ciudad);
-            grafoCiudad.AgregarVertice(ciudad);
-        }
-        else{
-            diccionario.Agregar(provicia, ciudad);
-            grafoCiudad.AgregarVertice(ciudad);
-        }
+        diccionario.Agregar(provincia, ciudad);
+        grafoCiudad.AgregarVertice(ciudad);
     }
+
+
     public void cargarCaminoCiudad(String ciudadOrigen , String ciudadDestino , int km){
         grafoCiudad.AgregarArista(ciudadOrigen, ciudadDestino, km);
     }
@@ -50,22 +51,55 @@ public class TPOciudad implements TPOciudadTDA{
             grafoCiudad.EliminarArista(ciudadOrigen, ciudadDestino);
         }
     }
+
     public void ciudadesVecinas(String ciudad){
-        
-    } // MOSTRAR LAS CIUDADES QUE CUMPLA LA CONDICION
-    public void ciudadesPuente(String ciudadOrigen , String ciudadDestino){
-        
+        System.out.println("----------Ciudades vecinas de " + ciudad + ":----------");
+        ColaStringTDA vecinos = grafoCiudad.Vertices();
+        while (!vecinos.ColaVacia()) {
+            String ciudadVecina = vecinos.Primero();
+            if (grafoCiudad.ExisteArista(ciudad, ciudadVecina) || grafoCiudad.ExisteArista(ciudadVecina, ciudad)) {
+                System.out.println(ciudadVecina);
+            }
+            vecinos.Desacolar();
+        }
     }
-    public void ciudadesPredecesoras (String ciudad){
-        
-    }// MOSTRAR LAS CIUDADES QUE CUMPLA LA CONDICION
+
+    public void ciudadesPuente(String ciudadOrigen, String ciudadDestino){
+        System.out.println("----------Ciudades puente entre " + ciudadOrigen + " y " + ciudadDestino + ":----------");
+        ColaStringTDA todasLasCiudades = grafoCiudad.Vertices();
+        while (!todasLasCiudades.ColaVacia()) {
+            String ciudadPuente = todasLasCiudades.Primero();
+            if (grafoCiudad.ExisteArista(ciudadOrigen, ciudadPuente) && grafoCiudad.ExisteArista(ciudadPuente, ciudadDestino)) {
+                System.out.println(ciudadPuente);
+            }
+            todasLasCiudades.Desacolar();
+        }
+    }
+
+    public void ciudadesPredecesoras(String ciudad){
+        System.out.println("----------Ciudades predecesoras de " + ciudad + ":----------");
+        ColaStringTDA todasLasCiudades = grafoCiudad.Vertices();
+        while (!todasLasCiudades.ColaVacia()) {
+            String ciudadPredecesora = todasLasCiudades.Primero();
+            if (grafoCiudad.ExisteArista(ciudadPredecesora, ciudad)) {
+                System.out.println(ciudadPredecesora);
+            }
+            todasLasCiudades.Desacolar();
+        }
+    }
+
+    // MOSTRAR LAS CIUDADES QUE CUMPLA LA CONDICION
     public void ciudadesExtremo(){
         
-    } // MOSTRAR LAS CIUDADES QUE CUMPLA LA CONDICION
+    }
+
+    // MOSTRAR LAS CIUDADES QUE CUMPLA LA CONDICION
     public void ciudadesFuertementeConectadas(){
         
-    } // MOSTRAR LAS CIUDADES QUE CUMPLA LA CONDICION
+    }
+
+    // MOSTRAR LAS CIUDADES QUE CUMPLA LA CONDICION
     public void camino(String ciudadOrigen , String ciudadDestino){
         
-    } // MOSTRAR LOS KM RECORRIDOS Y LAS CIUDADES PUENTES DE A VER
+    }
 }
---
+++ TPOciudad/src/main/App.java
@@ -0,0 +1,71 @@
+package main;
+
+import api.*;
+import impl.*;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        TPOciudadTDA t1 = new TPOciudad();
+
+        // Cargando ciudades
+        t1.cargarCiudades("Buenos Aires", "La Plata");
+        t1.cargarCiudades("Buenos Aires", "Mar del Plata");
+        t1.cargarCiudades("Buenos Aires", "CABA");
+        t1.cargarCiudades("Buenos Aires", "Tandil");
+
+        t1.cargarCiudades("Córdoba", "Ciudad de Córdoba");
+        t1.cargarCiudades("Córdoba", "Río Cuarto");
+        t1.cargarCiudades("Córdoba", "Villa Carlos Paz");
+
+        t1.cargarCiudades("Salta", "Cafayate");
+
+        t1.cargarCiudades("Chubut", "Rawson");
+        t1.cargarCiudades("Chubut", "Trelew");
+        t1.cargarCiudades("Chubut", "Puerto Madryn");
+
+        // Mostrar ciudades y provincias
+        System.out.println("Ciudades y Provincias:");
+        t1.listarCiudad();
+        t1.listarProvincias();
+
+        // Cargando caminos
+        t1.cargarCaminoCiudad("CABA", "Mar del Plata", 400);
+        t1.cargarCaminoCiudad("CABA", "La Plata", 60);
+        t1.cargarCaminoCiudad("CABA", "Tandil", 350);
+        t1.cargarCaminoCiudad("CABA", "Ciudad de Córdoba", 1300);
+
+        t1.cargarCaminoCiudad("Mar del Plata", "CABA", 500);
+        t1.cargarCaminoCiudad("Mar del Plata", "Ciudad de Córdoba", 1800);
+
+        t1.cargarCaminoCiudad("La Plata", "Ciudad de Córdoba", 1500);
+        t1.cargarCaminoCiudad("La Plata", "Rawson", 2700);
+
+        t1.cargarCaminoCiudad("Tandil", "CABA", 480);
+
+        t1.cargarCaminoCiudad("Ciudad de Córdoba", "Rawson", 2800);
+        t1.cargarCaminoCiudad("Ciudad de Córdoba", "Río Cuarto", 200);
+
+        t1.cargarCaminoCiudad("Río Cuarto", "Puerto Madryn", 1150);
+
+        t1.cargarCaminoCiudad("Villa Carlos Paz", "Ciudad de Córdoba", 40);
+        t1.cargarCaminoCiudad("Villa Carlos Paz", "Río Cuarto", 250);
+        t1.cargarCaminoCiudad("Villa Carlos Paz", "Trelew", 1400);
+
+        t1.cargarCaminoCiudad("Rawson", "Villa Carlos Paz", 1200);
+        t1.cargarCaminoCiudad("Rawson", "Cafayate", 2200);
+        t1.cargarCaminoCiudad("Rawson", "Trelew", 20);
+
+        // Mostrar ciudades y provincias después de cargar caminos
+        System.out.println("\nCiudades y Provincias después de cargar caminos:");
+        t1.listarCiudad();
+        t1.listarProvincias();
+
+        // Métodos agregados
+        t1.ciudadesVecinas("CABA");
+
+        t1.ciudadesPuente("Mar del Plata", "Ciudad de Córdoba");
+
+        t1.ciudadesPredecesoras("Rawson");
+    }
+}
