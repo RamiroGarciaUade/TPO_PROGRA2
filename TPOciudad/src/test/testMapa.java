bdc60b0e429fc9d0178e943c629a385d3bb1be2e
Ezequiel Luberto <eluberto@uade.edu.ar>
2024-06-14 21:28:59 -0300

    testCargaAristas

--- TPOciudad/src/test/testMapa.java
+++ TPOciudad/src/test/testMapa.java
@@ -44,12 +44,52 @@ public class testMapa {
         aux_ciudades.Desacolar();
         
         assert(aux_provincias.Primero().equals("BUENOS AIRES")||aux_provincias.Primero().equals("MENDOZA"));
+
+        
         
 
     }
 
     @Test
+    public void testCargaAristas(){
+        MapaTDA test = new Mapa();
+        ColaStringTDA aux = new ColaStringDinamica();
+        aux.InicializarCola();
+        test.InicializarMapa();
+        
+        test.cargarCiudades("BUENOS AIRES","CABA");
+        test.cargarCiudades("BUENOS AIRES","LANUS");
+        test.cargarCiudades("BUENOS AIRES","LA PLATA");
+        test.cargarCiudades("CORDOBA","CORDOBA");
+        test.cargarCiudades("CHUBUT","TRELEW");
+        test.cargarCiudades("CATAMARCA","BELEN");
+
+        test.cargarCaminoCiudad("CABA", "LANUS", 14);
+        test.cargarCaminoCiudad("LA PLATA", "CORDOBA", 1500);
+        test.cargarCaminoCiudad("TRELEW", "LA PLATA", 2000);
+        
+        aux=test.ciudadesVecinas("CABA");
+
+        assert((aux.Primero().equals("LANUS")));
+
+        aux=test.ciudadesVecinas("LANUS");
+
+        assert((aux.Primero().equals("CABA")));
+
+        aux=test.ciudadesVecinas("CORDOBA");
+
+        assert(aux.Primero().equals("LA PLATA"));
+
+        aux=test.ciudadesVecinas("LA PLATA");
+        
+        assert(aux.Primero().equals("TRELEW")||aux.Primero().equals("CORDOBA"));
+
+        aux=test.ciudadesVecinas("BELEN");
+
+        assert(aux.ColaVacia());
     
+    }
+
 
 
 }
